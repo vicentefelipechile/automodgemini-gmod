8146c7d5d735135e37d076754f11fe601a22b41b
vicentefelipechile <vicente@mapping-latam.cl>
2024-02-26 14:08:49 -0300

    Actualización del módulo Sandbox de Google Gemini Automod

--- lua/gemini/sh_sandbox.lua
+++ lua/gemini/sh_sandbox.lua
@@ -1,5 +1,5 @@
 --[[----------------------------------------------------------------------------
-                      Google Gemini Automod - Sandbox Hooks
+                     Google Gemini Automod - Sandbox Module
 ----------------------------------------------------------------------------]]--
 
 Gemini:AddHook("PlayerDeath", function(ply)
@@ -23,4 +23,28 @@ Gemini:AddHook("PostGamemodeLoaded", function()
             end
         end
     end)
+end)
+
+local ListPlayerUse = {}
+Gemini:AddHook("PlayerUse", function(ply, ent)
+    if not IsValid(ent) then return end
+    if not ListPlayerUse[ply] then
+        ListPlayerUse[ply] = ent
+        hook.Run("OnPlayerStartUseEntity", ply, ent)
+    end
+end)
+
+Gemini:AddHook("Think", function()
+    for _, ply in ipairs(ListPlayerUse) do
+        -- Check if their eyetrace is hitting the same entity
+        if IsValid(ply) and IsValid(ListPlayerUse[ply]) then
+            local tr = ply:GetEyeTrace()
+            if not tr.Entity or tr.Entity ~= ListPlayerUse[ply] then
+                ListPlayerUse[ply] = nil
+                hook.Run("OnPlayerStopUseEntity", ply, ListPlayerUse[ply])
+            end
+        else
+            ListPlayerUse[ply] = nil
+        end
+    end
 end)
