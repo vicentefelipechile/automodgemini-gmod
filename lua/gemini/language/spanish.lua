b062c36f88c2b7ac534b859059da9b5d7697c2e5
vicentefelipechile <vicente@mapping-latam.cl>
2024-03-03 21:04:36 -0300

    spanish

--- lua/gemini/language/spanish.lua
+++ lua/gemini/language/spanish.lua
@@ -1,5 +1,9 @@
+--[[----------------------------------------------------------------------------
+                         Google Gemini Automod - Spanish
+----------------------------------------------------------------------------]]--
+
 --[[------------------------
-       Spanish phrases
+       Util Functions
 ------------------------]]--
 
 local function GetEntityName(ent)
@@ -15,6 +19,10 @@ local function GetEntityName(ent)
         return ent.PrintName or ent:GetClass()
     end
 end
+
+--[[------------------------
+          Language
+------------------------]]--
 
 local LANG = Gemini:CreateLanguage("Spanish")
 
@@ -43,6 +51,11 @@ Gemini:AddPhrase(LANG, "PhysgunDrop", [[El jugador "%s" solto la entidad "%s" co
 Gemini:AddPhrase(LANG, "PlayerSay", [[El jugador "%s" dijo "%s" cerca de %s en las coordenadas %s.]])
 Gemini:AddPhrase(LANG, "PlayerDisconnected", [[El jugador "%s" se fue del servidor.]])
 Gemini:AddPhrase(LANG, "PlayerSilentDeath", [[El jugador "%s" se murio silenciosamente.]])
+
+
+--[[------------------------
+            Hooks
+------------------------]]--
 
 local DamageType = {
     [-1]            = "algo que no se puede determinar",
@@ -139,7 +152,9 @@ Gemini:OverrideHookLanguage(LANG, {
             Owner = ent:Getowning_ent()
         end
 
-        return {ent:GetClass(), IsValid(Owner) and Owner:IsPlayer() and Owner:Name() or "no tiene", IsValid(Owner) and Owner:IsPlayer() and (", su dueño es " .. Owner:Name()) or ""}
+        local OwnerName = IsValid(Owner) and Owner:IsPlayer() and Owner:Name() or "no tiene"
+
+        return {ent:GetClass(), OwnerName, IsValid(Owner) and Owner:IsPlayer() and (", su dueño es " .. Owner:Name()) or ""}
     end,
     ["PlayerEnteredVehicle"] = function(ply, vehicle)
         local EntPos = vehicle:GetPos()
@@ -199,3 +214,111 @@ Gemini:OverrideHookLanguage(LANG, {
         return {ply:Name()}
     end
 })
+
+--[[------------------------
+   Gamemodes Descriptions
+------------------------]]--
+
+Gemini:AddPhrase(LANG, "default", [[Título del Modo de Juego:
+Ningún Modo de Juego Conocido
+
+Descripcion:
+El siguiente modo de juego no tiene ningun objetivo conocido, por lo que es recomendable aplicar las reglas y descripciones del modo de juego Sandbox, ya que es el modo de juego por defecto y usado como base para la mayoria de los modos de juego.
+En el Sandbox los jugadores pueden construir o tambien pelear entre ellos, por lo que se deja a la imaginación de los jugadores, por lo cual el comportamiento de estos no estan establecidos o no hay ninguno que se pueda tomar de referencia.
+
+Objetivo:
+No hay ningun objetivo conocido, por lo que se recomienda aplicar las reglas y descripciones del modo de juego Sandbox.
+
+Mecanicas Conocidas:
+- Construcción de estructuras
+- Peleas entre jugadores
+- Uso de armas y herramientas
+- Uso de entidades y vehiculos
+
+Mapas/Entornos:
+El entorno o los mapas seleccionados por este modo de juego no tienen un objetivo conocido, por lo que se asumira los mapas como entornos de construcción (Con lugares abiertos o cerrados) o peleas entre jugadores.
+
+Comportamiento de los jugadores:
+El comportamiento de los jugadores no esta establecido, por lo que se asumira que los jugadores pueden hacer lo que quieran, siempre y cuando no rompan las reglas establecidas por el propio servidor.
+
+Reglas:
+Las unicas reglas conocidas son las reglas establecidas por el servidor, por lo que se recomienda seguir las reglas establecidas por el servidor o por el administrador del servidor.]])
+
+Gemini:AddPhrase(LANG, "sandbox", [[Título del Modo de Juego:
+Sandbox
+
+Descripcion:
+El modo de juego Sandbox es un modo de juego donde los jugadores pueden hacer lo que quieran, como construir, pelear, usar armas, usar herramientas, usar entidades y vehiculos, entre otras cosas. Este modo de juego se resume en una caja de arena donde los jugadores pueden hacer lo que quieran, siempre y cuando no rompan las reglas establecidas por el servidor o por el administrador del servidor.
+
+Objetivo:
+El Sandbox no tiene ningun objetivo, los jugadores solo haran lo que quieran como construir o matarse entre si.
+
+Mecanicas Conocidas:
+- Construcción de estructuras
+- Peleas entre jugadores
+- Uso de armas y herramientas
+- Uso de entidades y vehiculos
+- Noclip (Volar por el mapa)
+
+Mapas/Entornos:
+El entorno o los mapas seleccionados son por lo general de mundo abierto o tambien son estructuras diseñadas para explorar y construir. La mayoría de estos mapas tienen una estetica como una ciudad, un campo de pasto verde o incluso lugares abstractos.
+
+Comportamiento de los jugadores:
+El comportamiento de los jugadores cambia dependiendo de la situacion, por ejemplo si los jugadores desean construir, estos se comportaran de manera pacifica, pero si los jugadores desean pelear, estos se comportaran de manera agresiva.
+
+Reglas:
+Las reglas del Sandbox son establecidas por el servidor o por el administrador del servidor, por lo que se recomienda seguir las reglas establecidas por el servidor o por el administrador del servidor.]])
+
+Gemini:AddPhrase(LANG, "darkrp", [[Título del Modo de Juego:
+DarkRP
+
+Descripcion:
+DarkRP es un modo de juego basado en el modo de juego Sandbox, pero con la diferencia de que este modo de juego tiene un sistema de economia, trabajos, salarios, entre otras cosas. Este modo de juego es muy popular en Garry's Mod, ya que es un modo de juego que simula la vida real, por lo que los jugadores pueden simular tener una vida y dentro del contexto de que estan en un roleplay.
+
+Objetivo:
+El objetivo de DarkRP es simular una vida real, por lo que los jugadores pueden hacer lo que quieran, pero siempre y cuando esten dentro del contexto de que estan en un roleplay.
+
+Mecanicas Conocidas:
+- Casas y propiedades
+- Trabajos y salarios
+- Sistema de economia
+- Policias y criminales
+- Vehiculos y entidades
+- Robo de casas y propiedades
+
+Mapas/Entornos:
+Los mapas preferiblemente son de ciudades o lugares urbanos, ya que estos mapas son los mas adecuados para simular una vida real. Los mapas pueden tener casas, tiendas, calles, entre otras cosas.
+
+Comportamiento de los jugadores:
+El comportamiento de los jugadores depende del trabajo que tengan, por ejemplo si un jugador es policia, este se enfocara de buscar criminales o mantener el orden, pero si un jugador es un criminal, este se enfocara en robar casas o propiedades, tambien existen los trabajos civiles, que estos se enfocaran en trabajar para ganar dinero.
+
+Reglas:
+Existen reglas predefinidad por el modo de juego, como por ejemplo:
+- RDM (Random Deathmatch): Matar a alguien sin razon alguna.
+- NLR (New Life Rule): Esta regla es algo compleja, pero se resume en que si un jugador muere, este tiene que olvidar todo lo que paso antes de morir, por ejemplo si le han robado la casa, no puede volver inmediatamente o durante el robo.
+- FailRP: Hacer algo que no tiene sentido o que no esta dentro del contexto de que estan en un roleplay, como un policia robando a un civil.
+Y tambien existen reglas establecidas por el servidor o por el administrador del servidor, por lo que se recomienda seguir las reglas establecidas por el servidor o por el administrador del servidor.]])
+
+Gemini:AddPhrase(LANG, "terrortown", [[Título del Modo de Juego:
+Trouble in T Town
+
+Descripcion:
+Trouble in T Town es un modo de juego basado en el modo de juego Sandbox, pero con la diferencia de que este modo de juego tiene un sistema de roles, donde los jugadores son asignados a un rol especifico, como por ejemplo un inocente, un detective o un traidor. Este modo de juego es muy popular en Garry's Mod, ya que es un modo de juego que simula una situacion estilo among us, donde los jugadores tienen que descubrir quien es el traidor.
+
+Objetivo:
+El objetivo de Trouble in T Town es descubrir quien es el traidor, por lo que los jugadores tienen que trabajar en equipo para descubrir quien es el traidor y matarlo.
+
+Mecanicas Conocidas:
+- Detectives e inocentes
+- Traidores
+- Herramientas para los detectives y traidores
+- Armas para todos
+
+Mapas/Entornos:
+No existe un entorno especifico, ya que los mapas son muy variados pero todos son hechos para adaptarse al modo de juego, por lo que los mapas no estan limitados a un entorno especifico.
+
+Comportamiento de los jugadores:
+El comportamiento de los jugadores depende del rol que tengan, por ejemplo si un jugador es un detective, este se enfocara en buscar al traidor, pero si un jugador es un traidor, este se enfocara en matar a los inocentes o al detective, mientras que los inocentes ayudaran al detective a buscar al traidor.
+
+Reglas:
+Las reglas de Trouble in T Town son establecidas por el servidor o por el administrador del servidor, por lo que se recomienda seguir las reglas establecidas por el servidor o por el administrador del servidor.]])
