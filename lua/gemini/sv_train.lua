499f1799900cb05253c0c52df543eae68361b92c
vicentefelipechile <vicente@mapping-latam.cl>
2024-03-08 14:03:32 -0300

    Update sv_train.lua

--- lua/gemini/sv_train.lua
+++ lua/gemini/sv_train.lua
@@ -1,3 +1,44 @@
 --[[----------------------------------------------------------------------------
                       Google Gemini Automod - Train Module
 ----------------------------------------------------------------------------]]--
+
+--[[------------------------
+        SQL Database
+------------------------]]--
+
+Gemini.__TRAIN_SQL = {
+    ["GEMINI_TRAIN"] = [[
+        CREATE TABLE IF NOT EXITS gemini_train (
+            train_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            train_gamemode TEXT NOT NULL DEFAULT 'default',
+            train_data TEXT NOT NULL,
+            train_result TEXT NOT NULL
+        )
+    ]],
+    ["INSERT"] = [[
+        INSERT INTO
+            gemini_train (train_gamemode, train_data, train_result)
+        VALUES
+            ('%s', '%s', '%s')
+    ]],
+    ["GETALL"] = [[
+        SELECT
+            *
+        FROM
+            gemini_train
+        WHERE
+            train_gamemode = '%s'
+    ]],
+    ["GET"] = [[
+        SELECT
+            train_data,
+            train_result
+        FROM
+            gemini_train
+        WHERE
+            train_id = '%s' AND
+            train_gamemode = '%s'
+        LIMIT
+            %s
+    ]]
+}
